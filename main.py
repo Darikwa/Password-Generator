8ce82cf2f6ea026a70d24ce9434800811039b2a7
Darikwa <>
2021-06-02 14:30:23 +0000

    Removed variable that held len function to count the number of characters of the password

--- main.py
+++ main.py
@@ -2,19 +2,13 @@
 import random
 import data
 
-print("Welcome to the PyPassword Generator!")
-# nr_letters= int(input("How many characters?\n"))
-
+print("Random Password Generator!\n")
 
 password = []
 for _ in range(1, random.randint(18,25)):
   password.append(random.choice(data.letters))
 random.shuffle(password)
 password =''.join(password)
-print(password)
-pass_lenght=(len(password))
+print(f'Your Password is:\n{password}\n')
 
-print(f"Your password has: {pass_lenght} characters\n")
-
-# r_characters = random.randint(18,25)
-# print(r_characters)
+print(f"Your password has: {len(password)} characters\n\nThanks You 😊")
