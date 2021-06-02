918f7a6d6b813bc9d75ae96858e8607e2b0b6c4e
Darikwa <>
2021-06-02 14:23:45 +0000

    Removed the input prompt: Now generates random pass with char between 18-25 chars

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import random
 import data
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many characters?\n"))
-print('Your password is: \n')
+# nr_letters= int(input("How many characters?\n"))
+
 
 password = []
-for _ in range(1, nr_letters + 1):
+for _ in range(1, random.randint(18,25)):
   password.append(random.choice(data.letters))
 random.shuffle(password)
 password =''.join(password)
 print(password)
+pass_lenght=(len(password))
+
+print(f"Your password has: {pass_lenght} characters\n")
+
+# r_characters = random.randint(18,25)
+# print(r_characters)
